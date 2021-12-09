ff6808d33c2d55c4dbb5ad8efc1fbb6f3e75eb2e
lucia22navarro <64093863+lucia22navarro@users.noreply.github.com>
2021-12-09 17:15:26 +0100

    Update passport-setup.js

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -12,7 +12,6 @@ passport.deserializeUser(function(user, done) {
 });
 
 passport.use(new GoogleStrategy({
-    //consumerSecret: "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",
   },
   function(token, tokenSecret, profile, done) {
       //User.findOrCreate({ googleId: profile.id }, function (err, user) {
