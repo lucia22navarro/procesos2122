420c2f51ac67719366506b4b3e7c92cf75d9ac4e
lucia22navarro <64093863+lucia22navarro@users.noreply.github.com>
2021-12-09 17:15:13 +0100

    Update passport-setup.js

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -12,11 +12,7 @@ passport.deserializeUser(function(user, done) {
 });
 
 passport.use(new GoogleStrategy({
-    //consumerKey:"1033113634921-r4dmuu6c5l1ujv69nsgb68486sstasal.apps.googleusercontent.com",
-    clientID : "1033113634921-r4dmuu6c5l1ujv69nsgb68486sstasal.apps.googleusercontent.com",
     //consumerSecret: "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",
-    clientSecret : "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",
-    callbackURL: "http://localhost:5000/google/callback"
   },
   function(token, tokenSecret, profile, done) {
       //User.findOrCreate({ googleId: profile.id }, function (err, user) {
